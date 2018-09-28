edb9205f6bde2390bdf314c30c2bf8e8dc6fac39
Abdul Hameed <ahameed@ahameed.remote.csb>
2018-09-28 11:11:17 -0400

    updated config files

--- src/main/java/io/swagger/server/api/verticle/DefaultApiImpl.java
+++ src/main/java/io/swagger/server/api/verticle/DefaultApiImpl.java
@@ -10,7 +10,7 @@ public class DefaultApiImpl implements DefaultApi {
 		//we will only implement this method for the demo
 		Customer mycust=new Customer();
 		mycust.setCategory("Gold");
-		mycust.setName("Real Customer name here");
+		mycust.setName("Abdul Hameed");
 		mycust.setRegion("EMEA");
 		mycust.setId(5);
 		return mycust;
